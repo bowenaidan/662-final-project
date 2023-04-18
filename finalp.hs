2fa21c3605dc5118b6f442f6a4b4cd7998d437bf
Valerie Hernandez <47468622+ValYourPal@users.noreply.github.com>
2023-04-18 16:11:23 -0500

    Some typeOf

--- finalp.hs
+++ finalp.hs
@@ -54,9 +54,53 @@ data TY where -- types
 --------------------------------
 -- Part 1: Type Checking ------- TODO: -- Decide on a language name
 --------------------------------
+-- implement a method that takes an expression from your language and returns its type given a context. If no type is found your method should return Nothing. 
+typeOf :: Cont -> T -> (Maybe TY)
+typeOf _ (Int _) = Just Num
+typeOf _ (Bool _) = Just Boolean
+typeOf cont (Id x) = lookup x cont
+typeOf cont (Add x y) = do {
+  (Num) <- typeOf cont x;
+  (Num) <- typeOf cont y;
+  return Num
+}
+typeOf cont (Sub x y) = do {
+  (Num) <- typeOf cont x;
+  (Num) <- typeOf cont y;
+  return Num
+}
+typeOf cont (Mult x y) = do {
+  (Num) <- typeOf cont x;
+  (Num) <- typeOf cont y;
+  return Num
+}
+typeOf cont (Div x y) = do {
+  (Num) <- typeOf cont x;
+  (Num) <- typeOf cont y;
+  return Num
+}
+typeOf cont (Pow x y) = do {
+  (Num) <- typeOf cont x;
+  (Num) <- typeOf cont y;
+  return Num
+}
 
-typeOf :: T -> Maybe TY
-typeOf (Int _) = Just Num
+typeOf cont (Between x y z) = do {
+  (Num) <- typeOf cont x;
+  (Num) <- typeOf cont y;
+  (Num) <- typeOf cont z;
+  return Boolean
+}
+typeOf cont (Lambda x y) = do {
+  (ty) <- typeOf ((x, Num):cont) y;
+  return (Arrow Num ty)
+}
+typeOf cont (App x y) = do {
+  (Arrow ty1 ty2) <- typeOf cont x;
+  (ty1) <- typeOf cont y;
+  return ty2
+}
+
 
 --------------------------------
 -- Part 2: Evaluation ----------
