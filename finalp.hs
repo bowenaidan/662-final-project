1433a1d8573ee45955a09fa1d1629f8b30b40c5d
Valerie Hernandez <47468622+ValYourPal@users.noreply.github.com>
2023-04-20 16:00:26 -0500

    Part 1 done, not 100% about Lambda, App and Bind

--- finalp.hs
+++ finalp.hs
@@ -59,48 +59,124 @@ typeOf :: Cont -> T -> (Maybe TY)
 typeOf _ (Int _) = Just Num
 typeOf _ (Bool _) = Just Boolean
 typeOf cont (Id x) = lookup x cont
+
 typeOf cont (Add x y) = do {
-  (Num) <- typeOf cont x;
-  (Num) <- typeOf cont y;
-  return Num
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Num, Num) -> Just Num
+    _ -> Nothing
 }
+
 typeOf cont (Sub x y) = do {
-  (Num) <- typeOf cont x;
-  (Num) <- typeOf cont y;
-  return Num
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Num, Num) -> Just Num
+    _ -> Nothing
 }
+
 typeOf cont (Mult x y) = do {
-  (Num) <- typeOf cont x;
-  (Num) <- typeOf cont y;
-  return Num
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Num, Num) -> Just Num
+    _ -> Nothing
 }
+
 typeOf cont (Div x y) = do {
-  (Num) <- typeOf cont x;
-  (Num) <- typeOf cont y;
-  return Num
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Num, Num) -> Just Num
+    _ -> Nothing
 }
+
 typeOf cont (Pow x y) = do {
-  (Num) <- typeOf cont x;
-  (Num) <- typeOf cont y;
-  return Num
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Num, Num) -> Just Num
+    _ -> Nothing
 }
 
 typeOf cont (Between x y z) = do {
-  (Num) <- typeOf cont x;
-  (Num) <- typeOf cont y;
-  (Num) <- typeOf cont z;
-  return Boolean
-}
-typeOf cont (Lambda x y) = do {
-  (ty) <- typeOf ((x, Num):cont) y;
-  return (Arrow Num ty)
-}
-typeOf cont (App x y) = do {
-  (Arrow ty1 ty2) <- typeOf cont x;
-  (ty1) <- typeOf cont y;
-  return ty2
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  t3 <- typeOf cont z;
+  case (t1, t2, t3) of
+    (Num, Num, Num) -> Just Boolean
+    _ -> Nothing
 }
 
+typeOf cont (Lambda x y) = do {
+  t1 <- lookupVar x cont
+  -- type-check the body expression with the new variable in the context
+  t2 <- typeOf ((x, t1):cont) y
+  -- return the arrow type from t1 to t2
+  return (Arrow t1 t2)
+}
+
+typeOf cont (App x y) = do {
+  -- type-check the function expression
+  (Arrow t1 t2) <- typeOf cont x;
+  -- type-check the argument expression
+  t1' <- typeOf cont y;
+  if t1 == t1' -- if the type of the argument matches the domain of the function
+    then return t2
+    else Nothing
+}
+
+typeOf cont (Bind x y z) = do {
+  -- type-check the bound expression
+  t1 <- typeOf cont y;
+  -- type-check the body expression with the new variable in the context
+  t2 <- typeOf ((x, t1):cont) z;
+  return t2
+}
+
+typeOf cont (If x y z) = do {
+  -- type-check the condition expression
+  t1 <- typeOf cont x;
+  -- type-check the then expression
+  t2 <- typeOf cont y;
+  -- type-check the else expression
+  t3 <- typeOf cont z;
+  if t1 == Boolean && t2 == t3 -- if the condition is boolean and the then and else expressions have the same type
+    then return t2
+    else Nothing
+}
+
+typeOf cont (And x y) = do {
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Boolean, Boolean) -> Just Boolean
+    _ -> Nothing
+}
+
+typeOf cont (Or x y) = do {
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Boolean, Boolean) -> Just Boolean
+    _ -> Nothing
+}
+
+typeOf cont (Leq x y) = do {
+  t1 <- typeOf cont x;
+  t2 <- typeOf cont y;
+  case (t1, t2) of
+    (Num, Num) -> Just Boolean
+    _ -> Nothing
+}
+
+typeOf cont (IsZero x) = do {
+  t1 <- typeOf cont x;
+  case t1 of
+    Num -> Just Boolean
+    _ -> Nothing
+}
 
 --------------------------------
 -- Part 2: Evaluation ----------
