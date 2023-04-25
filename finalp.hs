9f902b2bdbc6a354a0bcf6fb947a6b7cc9fed3f6
Valerie Hernandez <47468622+ValYourPal@users.noreply.github.com>
2023-04-25 15:03:44 -0500

    Compiles

--- finalp.hs
+++ finalp.hs
@@ -53,7 +53,8 @@ data TY where -- types
 
 data TV where
   NumV :: Int -> TV
-  ClosureV :: String -> T -> Cont -> TV
+  BoolV :: Bool -> TV
+  ClosureV :: String -> T -> EnvV -> TV
   deriving (Show, Eq)
 
 -- Context
@@ -68,8 +69,11 @@ lookupVar :: String -> Cont -> Maybe TY
 lookupVar _ [] = Nothing
 lookupVar x ((y, t):cont) = if x == y then Just t else lookupVar x cont
 
+useClosure :: String -> TV -> EnvV -> EnvV -> EnvV
+useClosure i v e _ = (i,v):e 
+
 --------------------------------
--- Part 1: Type Checking ------- TODO: -- Decide on a language name
+-- Part 1: Type Checking ------- -- Decide on a language name
 --------------------------------
 -- implement a method that takes an expression from your language and returns its type given a context. If no type is found your method should return Nothing. 
 typeOf :: Cont -> T -> (Maybe TY)
@@ -77,64 +81,63 @@ typeOf _ (Int _) = Just Num
 typeOf _ (Bool _) = Just Boolean
 typeOf cont (Id x) = lookupVar x cont
 
-typeOf cont (Add x y) = do {
+typeOf cont (Add x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Num, Num) -> Just Num
     _ -> Nothing
-}
+ 
 
-typeOf cont (Sub x y) = do {
+typeOf cont (Sub x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Num, Num) -> Just Num
     _ -> Nothing
-}
+ 
 
-typeOf cont (Mult x y) = do {
+typeOf cont (Mult x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Num, Num) -> Just Num
     _ -> Nothing
-}
+ 
 
-typeOf cont (Div x y) = do {
+typeOf cont (Div x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Num, Num) -> Just Num
     _ -> Nothing
-}
+ 
 
-typeOf cont (Pow x y) = do {
+typeOf cont (Pow x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Num, Num) -> Just Num
     _ -> Nothing
-}
+ 
 
-typeOf cont (Between x y z) = do {
+typeOf cont (Between x y z) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   t3 <- typeOf cont z;
   case (t1, t2, t3) of
     (Num, Num, Num) -> Just Boolean
     _ -> Nothing
-}
+ 
 
-typeOf cont (Lambda x y) = do {
+typeOf cont (Lambda x y) = do
   t1 <- lookupVar x cont
   -- type-check the body expression with the new variable in the context
   t2 <- typeOf ((x, t1):cont) y
   -- return the arrow type from t1 to t2
   return (Arrow t1 t2)
-}
 
-typeOf cont (App x y) = do {
+typeOf cont (App x y) = do  
   -- type-check the function expression
   (Arrow t1 t2) <- typeOf cont x;
   -- type-check the argument expression
@@ -142,17 +145,17 @@ typeOf cont (App x y) = do {
   if t1 == t1' -- if the type of the argument matches the domain of the function
     then return t2
     else Nothing
-}
+ 
 
-typeOf cont (Bind x y z) = do {
+typeOf cont (Bind x y z) = do  
   -- type-check the bound expression
   t1 <- typeOf cont y;
   -- type-check the body expression with the new variable in the context
   t2 <- typeOf ((x, t1):cont) z;
   return t2
-}
+ 
 
-typeOf cont (If x y z) = do {
+typeOf cont (If x y z) = do  
   -- type-check the condition expression
   t1 <- typeOf cont x;
   -- type-check the then expression
@@ -162,38 +165,38 @@ typeOf cont (If x y z) = do {
   if t1 == Boolean && t2 == t3 -- if the condition is boolean and the then and else expressions have the same type
     then return t2
     else Nothing
-}
+ 
 
-typeOf cont (And x y) = do {
+typeOf cont (And x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Boolean, Boolean) -> Just Boolean
     _ -> Nothing
-}
+ 
 
-typeOf cont (Or x y) = do {
+typeOf cont (Or x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Boolean, Boolean) -> Just Boolean
     _ -> Nothing
-}
+ 
 
-typeOf cont (Leq x y) = do {
+typeOf cont (Leq x y) = do  
   t1 <- typeOf cont x;
   t2 <- typeOf cont y;
   case (t1, t2) of
     (Num, Num) -> Just Boolean
     _ -> Nothing
-}
+ 
 
-typeOf cont (IsZero x) = do {
+typeOf cont (IsZero x) = do  
   t1 <- typeOf cont x;
   case t1 of
     Num -> Just Boolean
     _ -> Nothing
-}
+ 
 
 
 --------------------------------
@@ -201,128 +204,127 @@ typeOf cont (IsZero x) = do {
 --------------------------------
 
 eval :: EnvV -> T -> (Maybe TV) --call-by-value and static scooping
-eval _ (Int _) = Just (Int _)
-eval _ (Bool _) = Just (Bool _)
-eval _ (Id _) = Just (Id _)
+eval eV (Int x) = Just (NumV x)
+eval eV (Bool x) = Just (BoolV x)
+eval eV (Id x) = lookup x eV
 
-eval eV (Add x y) = do {
+eval eV (Add x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
     (NumV x'', NumV y'') -> Just (NumV (x'' + y''))
     _ -> Nothing
-}
+ 
 
-eval eV (Sub x y) = do {
+eval eV (Sub x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
     (NumV x'', NumV y'') -> Just (NumV (x'' - y''))
     _ -> Nothing
-}
+ 
 
-eval eV (Mult x y) = do {
+eval eV (Mult x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
     (NumV x'', NumV y'') -> Just (NumV (x'' * y''))
     _ -> Nothing
-}
+ 
 
-eval eV (Div x y) = do {
+eval eV (Div x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
-    (NumV x'', NumV y'') -> Just (NumV (x'' / y''))
+    (NumV x'', NumV y'') -> Just (NumV (x'' `div` y''))
     _ -> Nothing
-}
+ 
 
-eval eV (Pow x y) = do {
+eval eV (Pow x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
     (NumV x'', NumV y'') -> Just (NumV (x'' ^ y''))
     _ -> Nothing
-}
+ 
 
-eval eV (Between x y z) = do {
+eval eV (Between x y z) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   z' <- eval eV z;
   case (x', y', z') of
     (NumV x'', NumV y'', NumV z'') -> Just (BoolV (x'' <= y'' && y'' <= z''))
     _ -> Nothing
-}
+ 
 
 eval eV (Lambda x y) = Just (ClosureV x y eV)
 
-eval eV (App x y) = do {
+eval eV (App x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case x' of
-    (ClosureV x'' y'' eV') -> eval ((x'', y'):eV') y''
-    _ -> Nothing
-}
+    ClosureV i b e -> eval (useClosure i y' e eV) b
+ 
 
-eval eV (Bind x y z) = do {
+eval eV (Bind x y z) = do  
   y' <- eval eV y;
   eval ((x, y'):eV) z
-}
+ 
 
-eval eV (If x y z) = do {
+eval eV (If x y z) = do  
   x' <- eval eV x;
   case x' of
     (BoolV True) -> eval eV y
     (BoolV False) -> eval eV z
     _ -> Nothing
-}
+ 
 
-eval eV (And x y) = do {
+eval eV (And x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
     (BoolV x'', BoolV y'') -> Just (BoolV (x'' && y''))
     _ -> Nothing
-}
+ 
 
-eval eV (Or x y) = do {
+eval eV (Or x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
     (BoolV x'', BoolV y'') -> Just (BoolV (x'' || y''))
     _ -> Nothing
-}
+ 
 
-eval eV (Leq x y) = do {
+eval eV (Leq x y) = do  
   x' <- eval eV x;
   y' <- eval eV y;
   case (x', y') of
     (NumV x'', NumV y'') -> Just (BoolV (x'' <= y''))
     _ -> Nothing
-}
+ 
 
-eval eV (IsZero x) = do {
+eval eV (IsZero x) = do  
   x' <- eval eV x;
   case x' of
     (NumV x'') -> Just (BoolV (x'' == 0))
     _ -> Nothing
-}
+ 
 
 
 --------------------------------
 -- Part 3: Fixed Point Operator
 --------------------------------
-TODO: -- update ast
-TODO: -- update type checker
-TODO: -- update eval
+-- update ast
+-- update type checker
+-- update eval
 
 --------------------------------
--- Part 4: New Language Feature TODO: -- Decide on a new feature
+-- Part 4: New Language Feature -- Decide on a new feature
 --------------------------------
 
-TODO: -- update ast
-TODO: -- update type checker
-TODO: -- update eval
+-- update ast
+-- update type checker
+-- update eval
 
 --------------------------------
 -- Part 5: Interpretation ------
